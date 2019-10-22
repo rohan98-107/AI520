0dc560f238bdd8df88d718b3e541b5a3b3ab0f18
Alex Eng <engalexm@gmail.com>
2019-10-21 22:06:34 -0400

    update nb with mineCt data

--- Assignment2/data/tester.py
+++ Assignment2/data/tester.py
@@ -50,7 +50,7 @@ def totalComparisonGameDriver(dim, density, trials, useMineCount = False):
                 round(baseline_cumulative_rate/(i+1),2),round(brute_cumulative_rate/(i+1),2),round(la_cumulative_rate/(i+1),2),round(la_brute_cumulative_rate/(i+1),2)))
 
 def mineCountComparisonGameDriver(dim, density, trials, agent_choice):
-    print("total comparison, dim {}, density {}, trials {}, agent_choice: {}".format(dim,density,trials,agent_choice.__name__))
+    #print("total comparison, dim {}, density {}, trials {}, agent_choice: {}".format(dim,density,trials,agent_choice.__name__))
     num_mines = int(density*(dim**2))
     baseline_cumulative_time = 0
     baseline_cumulative_rate = 0
@@ -68,13 +68,20 @@ def mineCountComparisonGameDriver(dim, density, trials, agent_choice):
         baseline_cumulative_rate+=baselineAgent.mineFlagRate*100
         mine_cnt_cumulative_time+=mine_cnt_agent.totalSolveTime
         mine_cnt_cumulative_rate+=mine_cnt_agent.mineFlagRate*100
+        '''
         if i % 10 == 9:
             print('\n\n\n\n\nFinished {} trials:'.format(i+1))
             print('\t{}: {} seconds \tUsing mine cnt: {} seconds'.format(\
                 agent_choice.__name__,round(baseline_cumulative_time/(i+1),2),round(mine_cnt_cumulative_time/(i+1),2)))
             print('\t{}: {}% \tUsing mine cnt: {}%'.format(\
                 agent_choice.__name__,round(baseline_cumulative_rate/(i+1),2),round(mine_cnt_cumulative_rate/(i+1),2)))
+        '''
+
+    return (baseline_cumulative_rate/trials), (mine_cnt_cumulative_rate/trials), (baseline_cumulative_time/trials), (mine_cnt_cumulative_time/trials)
+
 
 # totalComparisonGameDriver(10,.3,1000)
 # linearAlgebraWithBruteGameDriver(10,.2,"lin_alg_with_brute")
-mineCountComparisonGameDriver(10,.2,1000,brute_force_agent)
+#mineCountComparisonGameDriver(10, .2, 1000, brute_force_agent)
+
+
