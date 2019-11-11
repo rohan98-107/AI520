01702f896725d345de66d3f15a8a0ebe8dddcb8e
Aakash Raman <aakashraman@Aakashs-MBP.home>
2019-11-11 14:40:32 -0500

    agent fix

--- Assignment3/Agent.py
+++ Assignment3/Agent.py
@@ -5,6 +5,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math
 sns.set(font_scale=1)
 from copy import deepcopy
 
@@ -14,11 +15,11 @@ class agent:
     # track total number of cell searches (and movements for movement-restricted agent)
     num_actions = 0
 
-    def __init__(self, landscape, rule):
+    def __init__(self, landscape, rule, start_i = -1, start_j = -1):
         self.ls = landscape
         d = self.ls.dim
 
-        if rule == 1 or rule == 2:
+        if rule == 1 or rule == 2 or rule == 3 or rule == 4:
             self.rule = rule
         else:
             print("Invalid rule, set to 1 by default")
@@ -28,6 +29,8 @@ class agent:
         for i in range(d):
             for j in range(d):
                 self.knowledge[i][j].setBelief(1/(d**2))
+        self.i = random.randint(0, self.ls.dim-1) if start_i == -1 else start_i
+        self.j = random.randint(0, self.ls.dim-1) if start_j == -1 else start_j
 
     def printBelief(self):
         size = len(self.knowledge)
@@ -56,7 +59,6 @@ class agent:
         plt.savefig('./imgs/belief_{}.png'.format(filename), dpi=500)
 
     def searchCell(self,cell): #search a landCell
-        self.num_actions += 1
         if not cell.isTarget():
             return False
         else:
@@ -88,45 +90,50 @@ class agent:
         return (1-self.ls.landscape[x][y].getTerrain())*self.knowledge[x][y].getBelief()
 
     def updateBelief(self,x,y):
-        if self.searchCell(self.ls.landscape[x][y]):
-            for i in range(self.ls.dim):
-                for j in range(self.ls.dim):
-                    self.knowledge[i][j].setBelief(0)
-            self.knowledge[x][y].setBelief(1)
-        else:
-            #P(H|E) = P(E|H)P(H)/P(E)
-            #H: Target in cell
-            #E: Target not found
-            curr_belief = self.knowledge[x][y].getBelief()
-            num = self.ls.landscape[x][y].getTerrain()*curr_belief
-            denom = self.probNotFound()
-            remainder = abs(curr_belief - (num/denom))
-            self.knowledge[x][y].setBelief(num/denom)
-            for i in range(self.ls.dim):
-                for j in range(self.ls.dim):
-                    if i == x and j == y:
-                        continue
-                    else:
-                        temp = self.knowledge[i][j].getBelief()
-                        self.knowledge[i][j].setBelief(temp + (temp*remainder)/(1-remainder))
 
+        #P(H|E) = P(E|H)P(H)/P(E)
+        #H: Target in cell
+        #E: Target not found
+        curr_belief = self.knowledge[x][y].getBelief()
+        num = self.ls.landscape[x][y].getTerrain()*curr_belief
+        denom = self.probNotFound()
+        remainder = abs(curr_belief - (num/denom))
+        self.knowledge[x][y].setBelief(num/denom)
+        for i in range(self.ls.dim):
+            for j in range(self.ls.dim):
+                if i == x and j == y:
+                    continue
+                else:
+                    temp = self.knowledge[i][j].getBelief()
+                    self.knowledge[i][j].setBelief(temp + (temp*remainder)/(1-remainder))
         return self.knowledge
 
-    def getMaxLikCell(self):
+    def getMaxLikCell(self,i,j):
         if self.rule == 1:
             #get max i for P(Target in Cell i)
             belief = np.array([[self.knowledge[i][j].getBelief() for j in range(self.ls.dim)] for i in range(self.ls.dim)])
             return np.unravel_index(belief.argmax(),belief.shape)
-        else:
+        elif self.rule == 2:
             #get max i for P(Target FOUND in Cell i)
             belief = [[self.knowledge[i][j].getBelief()*(1-self.ls.landscape[i][j].getTerrain()) for j in range(self.ls.dim)] for i in range(self.ls.dim)]
             belief = np.array(belief)
             return np.unravel_index(belief.argmax(),belief.shape)
+        elif self.rule == 3:
+            belief = [[self.knowledge[i][j].getBelief()*(1-self.ls.landscape[i][j].getTerrain()) for j in range(self.ls.dim)] for i in range(self.ls.dim)]
+            belief = np.array(belief)
+            return np.unravel_index(belief.argmax(),belief.shape)
+        else:
+            pass
 
     def findTarget(self):
-        i = random.randint(0, self.ls.dim-1); j = random.randint(0, self.ls.dim-1)
+        i = self.i; j = self.j
+        self.num_actions += 1
         while not self.searchCell(self.ls.landscape[i][j]):
             self.knowledge = self.updateBelief(i,j)
-            i,j = self.getMaxLikCell()
+            next_i,next_j = self.getMaxLikCell(i,j)
+            self.num_actions += 1
+            if self.rule == 3 or self.rule == 4:
+                self.num_actions += math.abs(i - next_i) + math.abs(j - next_j)
+            i,j = next_i,next_j
 
         return (i,j)
