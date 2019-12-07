49454829e3257f4816fe891b579d5e1903b22e7e
Alex Eng <engalexm@gmail.com>
2019-12-07 14:29:11 -0500

    add same padding to pp-ing

--- Assignment4/Colorization.py
+++ Assignment4/Colorization.py
@@ -22,17 +22,29 @@ def crop_cmatrix(cmat):
 
 # converts RGB color matrix to grayscale matrix
 def rgb_to_grayscale_cmatrix(cmat):
+    ret = np.zeros((cmat.shape[0], cmat.shape[1])).astype(np.uint8)
     for i in range(cmat.shape[0]):
         for j in range(cmat.shape[1]):
-            cmat[i][j] = 0.21*cmat[i][j][0] + 0.72*cmat[i][j][1] + 0.07*cmat[i][j][2]
+            ret[i][j] = (0.21*cmat[i][j][0] + 0.72*cmat[i][j][1] + 0.07*cmat[i][j][2]).astype(np.uint8)
 
-    return cmat
+    return ret
 
 # pads image by adding 1px, zero-valued border on all four sides,
 # making an (n,n) image into an (n+2, n+2) image
 def pad_cmatrix(cmat):
     return np.pad(cmat, ((1,1), (1,1), (0,0)), 'constant')
 
+# same-pad image, i.e. for smaller of l,w, repeatedly add last row/column until l=w
+def same_pad_cmatrix(cmat):
+    if cmat.shape[0] < cmat.shape[1]:
+        row = cmat[0]
+        for i in range(cmat.shape[0], cmat.shape[1]):
+            cmat = np.r_[cmat, [row]]
+    elif cmat.shape[0] > cmat.shape[1]:
+        col = cmat[:, -1]
+        for i in range(cmat.shape[1], cmat.shape[0]):
+            cmat = np.c_[cmat, col]
+    return cmat
 
 
 
@@ -47,14 +59,11 @@ img_filename = 'test1.png'
 # convert image to nxm matrix of (r,g,b) values
 cmat = img_to_cmatrix(img_filename)
 
-# crop nxm color matrix to nxn where n<=m
-cmat = crop_cmatrix(cmat)
-
 # convert color matrix to grayscale using: gray_val = 0.21*r + 0.72*g + 0.07*b
 cmat = rgb_to_grayscale_cmatrix(cmat)
 
-# pad image with zeroes on all borders
-cmat = pad_cmatrix(cmat)
+# same-pad image
+cmat = same_pad_cmatrix(cmat)
 
 # export pre-processed image
 cmatrix_to_img(cmat, 'preprocessed_' + img_filename)
